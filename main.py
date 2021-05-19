41db5d620748f006da6c170da05b68d91e60ec17
Tony <SjasFaceMD@gmail.com>
2021-05-19 23:41:01 +0300

    basic pose estimated
    Converted the code from gesture mouse project, fortunately made easy with mediapipe

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
 import cv2
-import numpy as np
+from pose_estimation import pose_tracker
 
 # TODO pose estimation module
 # TODO TTS module
@@ -15,3 +14,28 @@ import numpy as np
 # NOTE I guess I can unify the excercises somewhat
 # by counting the distance between
 # key points, like wrist to sholder for curls, chest to wrist for pushups etc
+
+
+cap = cv2.VideoCapture('jerma_walking.mp4')
+p_tracker = pose_tracker(debug_draw=True)
+try:
+    while cap.isOpened():
+        ret, frame = cap.read()
+        resized_frame = cv2.resize(frame, (640, 480))
+
+        # if frame is read correctly ret is True
+        if not ret:
+            print("Can't receive frame (stream end?). Exiting ...")
+            break
+        pose = p_tracker.find_poses(resized_frame)
+        if pose:
+            hand_data = p_tracker.get_landmark_data(resized_frame, pose)
+            frame = p_tracker.draw_debug_landmarks(resized_frame, pose)
+        cv2.imshow('frame', resized_frame)
+        if cv2.waitKey(1) == ord('q'):
+            break
+except cv2.error:
+    print("end")
+finally:
+    cap.release()
+    cv2.destroyAllWindows()
--- pose_estimation.py
+++ pose_estimation.py
@@ -1,2 +1,35 @@
 import mediapipe
 from numpy import ndarray
+
+
+class pose_tracker():
+    def __init__(self, debug_draw: bool = False, min_detection_confidence: float = 0.7, min_tracking_confidence: float = 0.5):
+        self.mp_pose = mediapipe.solutions.pose
+        self.pose = self.mp_pose.Pose(min_detection_confidence=min_detection_confidence,
+                                      min_tracking_confidence=min_tracking_confidence)
+        if debug_draw:
+            self.debug_draw = mediapipe.solutions.drawing_utils
+        else:
+            self.debug_draw = None
+
+    def find_poses(self, rgb_frame: ndarray):
+
+        result = self.pose.process(rgb_frame)
+        if result.pose_landmarks:
+            pose = result.pose_landmarks
+            return pose
+
+    def get_landmark_data(self, rgb_frame: ndarray, pose: list):
+
+        pose_data = {}
+        for landmark_id, landmark in enumerate(pose.landmark):
+            height, width, channels = rgb_frame.shape
+            cx, cy = int(landmark.x*width), int(landmark.y*height)
+            pose_data[landmark_id] = [landmark.x, landmark.y, landmark.z, cx, cy]
+        return pose_data
+
+    def draw_debug_landmarks(self, frame: ndarray, pose: list):
+
+        if self.debug_draw:
+            self.debug_draw.draw_landmarks(frame, pose, self.mp_pose.POSE_CONNECTIONS)
+        return frame
